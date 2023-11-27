8c79591f24017bb578a096d1a1acf0edb28c0ca0
Neel Chheda <130147345+neelchheda-rite@users.noreply.github.com>
2023-11-27 15:02:24 +0530

    Navbar + Filters

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import { Counter } from './features/counter/Counter';
+import { ProductList } from "./features/product-list/ProductList";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-    <Counter></Counter>
+      <ProductList />
     </div>
   );
 }
